a89fbb0bb37057e72e1abdba6970dbd11e5f26bd
123456466 <sunji7378@gmail.com>
2025-02-27 21:15:23 +0900

    fix : 프롭 전달

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,9 +1,10 @@
-const Input = ({ type, onClick, placeholder, className }) => {
+const Input = ({ type, onChange, placeholder, className, value }) => {
   return (
     <input
       type={type}
-      onClick={onClick}
+      onChange={onChange}
       placeholder={placeholder}
+      value={value}
       className={`w-[220px] h-[50px] px-[20px] rounded-full ${className}`}
     />
   );

dcfc07b76fd98ec1a3c9c649c43e35cb57b4e77b
Juha Yoon <ijooha16@gmail.com>
2025-02-27 14:28:47 +0900

    update: ui 라이브러리 기본 구성 완료

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,5 +1,15 @@
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ type, onClick, text }) => {
+  return (
+    <button type={type} onClick={onClick} className="bg-[#191970] font-semibold text-white h-[50px] px-[30px] rounded-full">
+      {text}
+    </button>
+  );
 };
 
 export default Button;
+
+//코랄 ff6161
+//핑크 
+//네이비 191970
+//블루 4682B4
+//크림 베이지 F5DEB3 FFF8DC

7a9b1ab86f400dd0ebd56b6872732dcf980079ae
Juha Yoon <ijooha16@gmail.com>
2025-02-28 15:31:10 +0900

    feat: 카페 카드(마이페이지, 검색 결과 리스트 페이지에 사용)

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,22 @@
+import useCafeStore from "../stores/useCafeStore";
+import ContentLayout from "./layout/ContentLayout";
+
 const Modal = ({ children }) => {
+  const { setSelectedCafe } = useCafeStore();
+
   return (
     <div
-      className="bg-[#eeeeee] flex flex-col items-center gap-[16px] flex flex-col p-[30px_20px] rounded-[30px]"
-      onClick={(e) => e.stopPropagation()}
+      onClick={() => setSelectedCafe(null)}
+      className="z-50 fixed flex justify-center top-0 left-0 w-screen h-screen bg-[#000000a8]"
     >
-      {children}
+      <ContentLayout>
+        <div
+          className="bg-[#eeeeee] flex flex-col items-center gap-[16px] flex flex-col p-[30px_20px] rounded-[30px]"
+          onClick={(e) => e.stopPropagation()}
+        >
+          {children}
+        </div>
+      </ContentLayout>
     </div>
   );
 };
--- src/components/common/ContentBox.jsx
+++ src/components/common/ContentBox.jsx
@@ -1,6 +1,6 @@
 const ContentBox = ({ children, className }) => {
   return (
-    <div className="bg-[#eeeeee] flex flex-col items-center gap-[16px] w-[400px] flex flex-col p-[30px_20px] rounded-[30px]">
+    <div className={`bg-[#eeeeee] flex flex-col items-center gap-[16px] w-[400px] flex flex-col p-[20px_16px] rounded-[30px] ${className}`}>
       {children}
     </div>
   );

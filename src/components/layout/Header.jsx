9e75ba4cbe32d8473fb20ba61ded43c4784d486a
123456466 <sunji7378@gmail.com>
2025-03-02 12:31:51 +0900

    feat : 조건부 렌더링 추가

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const userData = JSON.parse(localStorage.getItem("user-data"));
   return (
     <div className="h-[160px] bg-white w-full flex justify-between items-center px-[40px]">
       <div className="w-[220px]"></div>
@@ -8,8 +9,16 @@ const Header = () => {
         Logo
       </Link>
       <div className="w-[220px] flex gap-[40px]">
-        <div className="text-darkgray">마이페이지</div>
-        <Link to='/log-in' className="text-darkgray">로그인 / 회원가입</Link>
+        <Link to={userData ? "/my-page" : "/sign-up"} className="text-darkgray">
+          {userData ? "마이페이지" : "회원가입"}
+        </Link>
+        {userData ? (
+          <div className="text-darkgray">로그아웃</div>
+        ) : (
+          <Link to="/log-in" className="text-darkgray">
+            로그인
+          </Link>
+        )}
       </div>
     </div>
   );

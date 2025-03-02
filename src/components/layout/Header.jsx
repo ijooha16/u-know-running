929c1ce48ab49f006d84f0ae989ed7a271019e80
123456466 <sunji7378@gmail.com>
2025-03-02 20:14:57 +0900

    feat : 로그아웃 기능 추가

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import supabase from "../../services/supabase";
 
 const Header = () => {
   const userData = JSON.parse(localStorage.getItem("user-data"));
+
+  const onLogoutHandler = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      return toast.error("로그아웃 에러: " + error);
+    }
+
+    window.localStorage.removeItem("user-data");
+    window.location.replace("/");
+  };
+
   return (
     <div className="h-[160px] bg-white w-full flex justify-between items-center px-[40px]">
       <div className="w-[220px]"></div>
@@ -13,7 +26,9 @@ const Header = () => {
           {userData ? "마이페이지" : "회원가입"}
         </Link>
         {userData ? (
-          <div className="text-darkgray">로그아웃</div>
+          <button className="text-darkgray" onClick={onLogoutHandler}>
+            로그아웃
+          </button>
         ) : (
           <Link to="/log-in" className="text-darkgray">
             로그인

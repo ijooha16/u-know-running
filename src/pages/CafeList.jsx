2e2c8c1650852c3f74839c03ddb9769d13ca0156
noodlewd <michel35@naver.com>
2025-03-03 00:12:08 +0900

    test: 더미데이터를 추가하여 사용자만의 북마크 추가 삭제 기능 확인

--- src/pages/CafeList.jsx
+++ src/pages/CafeList.jsx
@@ -4,10 +4,13 @@ import ContentLayout from "../components/layout/ContentLayout";
 const CafeList = () => {
   return (
     <ContentLayout>
-      <div className="grid grid-cols-3 gap-[30px]">
+      {/* 랜더링 시 맵을 이용하는바람에 컬럼 1개당 3개씩 묶여있어 일단 flex로 임시 수정 grid로 변경해야할시 div flex 주석 후 아래의 grid 주석 해제 */}
+      <div className="flex flex-wrap gap-[30px] justify-start">
+        {/* <div className="grid grid-cols-3 gap-[30px]"> */}
         <CafeCard />
-        <CafeCard />
-        <CafeCard />
+        {/* 임시 더미데이터를 넣어서 랜더링 시 9개가나와 주석처리함 */}
+        {/* <CafeCard />
+        <CafeCard /> */}
       </div>
     </ContentLayout>
   );

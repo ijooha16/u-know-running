8085f3d7768f13b7f84d9b0fc6435ea5a37eaf27
123456466 <sunji7378@gmail.com>
2025-03-03 18:03:34 +0900

    feat : 인증관리 라우터 설정

--- src/routes/ProtectedRouter.jsx
+++ src/routes/ProtectedRouter.jsx
@@ -1,5 +1,11 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import useUserStore from "../stores/useUserStore";
+
 const ProtectedRouter = () => {
-  return <div>ProtectedRouter</div>;
+  const { userData } = useUserStore();
+  const { pathname } = useLocation;
+
+  return userData? (<Outlet/>) : (<Navigate to="/log-in" replace state={{redirectedFrom: pathname}} />);
 };
 
 export default ProtectedRouter;

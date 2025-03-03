fdea286bbb3654fdb695ef8a0e5ecbc249981479
123456466 <sunji7378@gmail.com>
2025-03-03 14:06:56 +0900

    feat : 회원가입시 로그인 정보 스토어에 저장하는 로직 작성

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,19 +4,20 @@ import Button from "../components/common/Button";
 import Input from "../components/common/Input";
 import ContentBox from "../components/common/ContentBox";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import useUserStore from "../stores/useUserStore";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [nickname, setNickname] = useState("");
   const navigate = useNavigate();
+  const { setUserData } = useUserStore;
 
   const onSignupHandler = async (e) => {
     e.preventDefault();
 
     //수파베이스에 있는 회원가입 함수
-    const { error } = await supabase.auth.signUp({
+    const { data,error } = await supabase.auth.signUp({
       email,
       password,
       options: {
@@ -27,11 +28,10 @@ const SignUp = () => {
     });
     if (error) throw error;
 
-    //...그냥 트리거로 넘겼습니다 왜 안되는거야 저번에 되는거 확인 했는데
-
-    //회원가입 완료 후 알럿창과 함께 페이지 이동
-    toast.success('회원가입 완료!')
-    navigate('/splash')
+//회원가입시 자동으로 로그인 되면 그 정보를 스토어에 넘기는 로직
+    setUserData(data.user)
+//리로드 되어야 헤더의 로그인/회원가입이 바뀌어서 리로드 하면서 페이지 이동
+    window.location.replace("/splash")
   };
 
   return (

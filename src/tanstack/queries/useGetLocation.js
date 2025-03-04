37f73c76d446c7bd9bfd31ab20868d3f31254309
PureunKang <pureun0000@gmail.com>
2025-03-05 05:35:54 +0900

    docs: queryKey 상수화

--- src/tanstack/queries/useGetLocation.js
+++ src/tanstack/queries/useGetLocation.js
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getLocation } from "../../services/location";
+import { QUERY_KEYS } from "../query.keys";
 
 const useGetLocation = () => {
   return useQuery({
-    queryKey: ["userLocation"],
+    queryKey: QUERY_KEYS.LOCATION,
     queryFn: getLocation,
     staleTime: 1000 * 60 * 5, // 5분 동안 캐싱
     retry: false // 위치 가져오기에 실패하면 재시도 안 함
--- src/tanstack/queries/useUserInfo.js
+++ src/tanstack/queries/useUserInfo.js
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import supabase from "../../services/supabase";
+import { QUERY_KEYS } from "../query.keys";
 
 export const useUserInfo = () => {
   return useQuery({
-    queryKey: ["userInfo"], // 쿼리 키는 배열 형태로 전달
+    queryKey: QUERY_KEYS.USER_INFO, // 쿼리 키는 배열 형태로 전달
     queryFn: async () => {
       const { data: authUser, error: authError } = await supabase.auth.getUser();
       if (authError || !authUser?.user) {

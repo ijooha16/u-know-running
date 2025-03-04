8780389f3110604001a4a4da55da9e0b7969c599
123456466 <sunji7378@gmail.com>
2025-03-04 23:21:32 +0900

    feat : 이름 누르면 깃허브로 이동

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,16 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <div className="h-[200px] w-full bg-[#eeeeee] flex flex-col justify-center items-center">
-      <p className="text-darkgray" >Made by Team CloudNine</p>
-      <div className="flex">
-        <div className="text-darkgray">
-          <p>윤주하</p>
-          <p>김동우</p>
-          <p>강푸른</p>
+      <p className="text-darkgray mb-3">Made by Team CloudNine</p>
+      <div className="flex row">
+        <div className="text-darkgray mr-3 flex flex-col">
+          <Link to="https://github.com/ijooha16" target="_blank" >윤주하</Link>
+          <Link to="https://github.com/noodlewd" target="_blank" >김동우</Link>
+          <Link to="https://github.com/PureunKang" target="_blank" >강푸른</Link>
         </div>
-        <div className="text-darkgray">
-          <p>이수민</p>
-          <p>최지선</p>
+        <div className="text-darkgray ml-3 flex flex-col">
+          <Link to="https://github.com/Sumin-Lee12" target="_blank" >이수민</Link>
+          <Link to="https://github.com/123456466" target="_blank" >최지선</Link>
         </div>
       </div>
     </div>

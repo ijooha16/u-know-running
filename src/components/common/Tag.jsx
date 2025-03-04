803e5a75d9befc284bdd7236eb1ff67162faecb8
Juha Yoon <ijooha16@gmail.com>
2025-03-05 00:00:29 +0900

    design: 목록 리스트 태그 수정

--- src/components/common/Tag.jsx
+++ src/components/common/Tag.jsx
@@ -1,9 +1,9 @@
-const Tag = ({ tagText, onClick, isSelected }) => {
+const Tag = ({ tagText, onClick, isSelected, className }) => {
   return (
     <div
       onClick={onClick}
-      className={`flex items-center h-[30px] px-[16px] bg-lightgray text-[14px] text-darkgray rounded-full ${
-        isSelected ? "bg-primary" : "bg-lightgray"
+      className={`flex items-center px-[16px] bg-lightgray rounded-full ${className} ${
+        isSelected ? "bg-primary text-white text-[18px] h-[50px]" : "bg-lightgray text-darkgray text-[14px] h-[30px]"
       }`}
     >
       # {tagText}
@@ -12,10 +12,3 @@ const Tag = ({ tagText, onClick, isSelected }) => {
 };
 
 export default Tag;
-
-// 내가 수정하기 전 원본 Tag 코드
-// const Tag = ({tagText}) => {
-//   return <div className="flex items-center h-[30px] px-[16px] bg-white text-[14px] text-darkgray rounded-full"># {tagText}</div>;
-// };
-
-// export default Tag;

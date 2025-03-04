dd7ca910c5ca506727fe99f28239131144b1f654
123456466 <sunji7378@gmail.com>
2025-03-04 16:54:07 +0900

    design : 기본적인 레이아웃 잡기

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,20 @@
 const Footer = () => {
-  return <div className="h-[260px] w-full bg-[#eeeeee] flex flex-col justify-center items-center">Footer</div>;
+  return (
+    <div className="h-[200px] w-full bg-[#eeeeee] flex flex-col justify-center items-center">
+      <p className="text-darkgray" >Made by Team CloudNine</p>
+      <div className="flex">
+        <div className="text-darkgray">
+          <p>윤주하</p>
+          <p>김동우</p>
+          <p>강푸른</p>
+        </div>
+        <div className="text-darkgray">
+          <p>이수민</p>
+          <p>최지선</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;

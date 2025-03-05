52784e6e8a7a3640733044710bd2e1ad594a3fcf
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-05 13:48:47 +0900

    design: 병합 후 디자인 재수정

--- src/components/CafeCard.jsx
+++ src/components/CafeCard.jsx
@@ -5,6 +5,7 @@ import useUserStore from "../stores/useUserStore";
 import { toast } from "react-toastify";
 import MainTag from "./common/MainTag";
 import { useGetCafeTopTags } from "../tanstack/queries/useGetCafeTags";
+import { useState, useEffect } from "react";
 
 const CafeCard = ({ cafe }) => {
   // 받아온걸로 데이터 추출
@@ -66,24 +67,22 @@ const CafeCard = ({ cafe }) => {
 
 
   return (
-    <div className={`shadow h-[400px]] w-[300px] flex flex-col items-center gap-[16px] text-white rounded-[20px]`}>
-      <div
-        key={cafe_id}
-        className="bg-gradient-to-t from-[#000000d7] to-[#0000003e] min-h-[300px] h-full w-full flex flex-col justify-start items-start p-[20px_16px] rounded-[20px]"
-      >
-        <img src={image} className="w-[268px] h-[268px] object-cover" />
-        <div className="flex justify-between w-full items-center pr-[12px] mt-[16px]">
-          <MainTag tagText={tagList[0]?.tag || "태그없음"} />
+    <div className={`shadow h-[450px] w-[300px] flex flex-col items-center gap-[16px] text-white rounded-[20px] overflow-hidden relative`}>
+    <img src={imageUrl} className="absolute h-[450px] min-w-fit" />
+    <div key={cafe_id} className="z-0 bg-gradient-to-t from-[#000000d7]/60 to-[#0000003e]/0 min-h-[300px] h-full w-full flex flex-col justify-start items-start p-[20px_16px] rounded-[20px]">
+      <div className="absolute bottom-10">
+        <div className="flex justify-between w-full items-center pr-[12px]">
           <Icon
             icon={isBookmarked ? "bookMark" : "bookMark_empty"}
             onClick={handleBookmarkToggle}
             className="cursor-pointer"
           />
         </div>
-        <div className="z-0 font-semibold text-[26px] pl-[12px] mt-[10px]">{place_name}</div>
+        <div className="z-0 font-semibold text-[26px] px-[12px] mt-[10px] outline-solid mb-2">{place_name}</div>
         <div className="z-0 text-lightgray text-[14px] pl-[12px]">{address_name || road_address_name}</div>
       </div>
     </div>
+  </div>
   );
 };
 
